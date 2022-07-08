0fb7329acba46a1c23af4e21dfafac8d95162496
Faktor 10 <oreillyross@gmail.com>
2022-07-08 16:26:00 +0200

    5 steps to UI development

--- src/App.js
+++ src/App.js
@@ -4,6 +4,122 @@
 // usually the entry point to your app
 import { useState } from "react";
 import "./styles.css";
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
+
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyCHange
+}) {
+  return (
+    <form>
+      <div>
+        <input
+          onChange={(e) => onFilterTextChange(e.target.value)}
+          type="text"
+          value={filterText}
+          placeholder="Search..."
+        />
+      </div>
+      <label>
+        <input
+          onChange={(e) => onInStockOnlyCHange(e.target.checked)}
+          checked={inStockOnly}
+          type="checkbox"
+        />{" "}
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+const ProductCategoryRow = ({ category }) => {
+  return (
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
+  );
+};
+
+const ProductRow = ({ product }) => {
+  return (
+    <tr>
+      <td>{product.name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+};
+
+function ProductTable({ products, filterText, inStockOnly }) {
+  const rows = [];
+
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          key={product.category}
+          category={product.category}
+        />
+      );
+    }
+    rows.push(<ProductRow key={product.name} product={product} />);
+
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      {/* You can pass an array of React components to be rendered */}
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyCHange={setInStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+}
+
 // export this piece of UI which can be imported later without the {}
 // only one default export allowed in a file
 export default function App() {
@@ -28,9 +144,19 @@ export default function App() {
       the event handler passes the call up to handleClick in parent component */}
       <MyButton count={count} handleClick={handleClick} />
       <Display />
+      <VegStore />
     </div>
   );
 }
+
+const VegStore = () => {
+  return (
+    <div>
+      <FilterableProductTable products={PRODUCTS} />
+    </div>
+  );
+};
+
 // A component is a piece of UI. Functions that return markup
 // The capital letter is important, distinguishes a React component from
 // regular HTML markup.
